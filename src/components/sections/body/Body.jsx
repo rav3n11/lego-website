60820035953faa11d367ded099b3f7665def0e02
Bereket Terefe <berelliant11@gmail.com>
2022-07-17 00:05:40 +0300

    [add] - items card section added.  - don't ask me about mobile responsiveness  - horizontal scroll required a package

--- src/components/sections/body/Body.jsx
+++ src/components/sections/body/Body.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import config from "./config";
 import HomeTabs from "../../tabs/Tabs";
+import ItemCard from "../../itemCard";
 import { Link } from "react-router-dom";
 // import Cup from '../../cup/Cup';
 import JSX_withParallax from "../../../util/ParallaxJSXWrapper";
 import batman from "../../../assets/icons/batman.svg";
 // import superman from "../../../assets/icons/lego-superman.svg";
 import yellow from "../../../assets/icons/yellow.svg";
+
+import pool from "../../../assets/icons/pool.svg";
+import hulk from "../../../assets/icons/hulk.svg";
+import spidy from "../../../assets/icons/spidy.svg";
+import supes from "../../../assets/icons/lego-superman.svg";
+import loki from "../../../assets/icons/loki.svg";
+
+import HorizontalScroll from "react-scroll-horizontal";
 export default function Body() {
   let title = config.title,
     description = config.description,
@@ -24,9 +33,9 @@ export default function Body() {
     <div id="about" className="fix">
       <div className="about-area ptb--120  bg_color--1">
         <div className="about-wrapper">
-        <div className="banner">
-                        <p className="banner__text">New Sets 2018</p>
-                      </div>
+          <div className="banner">
+            <p className="banner__text">New Sets 2018</p>
+          </div>
           <div className="circle__blue"></div>
           <div className="body-card">
             <div className="circle__blue">
@@ -43,10 +52,15 @@ export default function Body() {
                           <br />
                           {description}
                         </p>
-                      <div className="yellow-lego__parent">
-                        <img className="yellow-lego" src={yellow} id="yellow lego block" alt="Lego block" />
-                        <p className="yellow-lego-text">see collection</p>
-                      </div>
+                        <div className="yellow-lego__parent">
+                          <img
+                            className="yellow-lego"
+                            src={yellow}
+                            id="yellow lego block"
+                            alt="Lego block"
+                          />
+                          <p className="yellow-lego-text">see collection</p>
+                        </div>
                       </div>
                       {/* <div className="row mt--30">
                         <HomeTabs />
@@ -57,17 +71,59 @@ export default function Body() {
               </div>
             </div>
             <div className="about-socials">
-            <p className="media-title">Our social media</p>
-            <ul className="social-share social-style--2 color-black d-flex justify-content-start liststyle">
-                                {socials.map((val , i) => (
-                                    <li key={i}><Link to={`${val.link}`}>{val.Social} {val.data && <strong className='data'>{val.data}</strong>}</Link></li>
-                                ))}
-                            </ul>
+              <p className="media-title">Our social media</p>
+              <ul className="social-share social-style--2 color-black d-flex justify-content-start liststyle">
+                {socials.map((val, i) => (
+                  <li key={i}>
+                    <Link to={`${val.link}`}>
+                      {val.Social}{" "}
+                      {val.data && <strong className="data">{val.data}</strong>}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <div>
-              <Batman />
+          <div className="about-overlay">
+            <Batman />
+            <div className="horizontal-wrapper">
+              <HorizontalScroll reverseScroll={true}>
+                <div className="cards">
+                  <ItemCard
+                    title="Cpt. Deadpool"
+                    description="Merc with a mouth. This action figure is the only one knows about you. Some more lorem ipsum text here..."
+                    image={pool}
+                    color="red"
+                  />
+                  <ItemCard
+                    title="HULK"
+                    description="SMASH! By far the strongest avenger. Just don't let thor hear you say that. "
+                    image={hulk}
+                    color="green"
+                  />
+                  <ItemCard
+                    title="Spider Man"
+                    description="Something's tingling. Maybe its your sense of limitless adventure with this spiderman lego set."
+                    image={spidy}
+                    color="blue"
+                  />
+                  <ItemCard
+                    title="Loki"
+                    description="The God of mischeif. This set of the loki will make you feel like you're in a different universe. "
+                    image={loki}
+                    color="yellow"
+                  />
+                  <ItemCard
+                    title="Superman"
+                    description="Ready to win every game of top trumps ever. This leog set of the supes will surely accomplish that. "
+                    image={supes}
+                    img_size="small"
+                    color="red"
+                  />
+                </div>
+              </HorizontalScroll>
             </div>
+          </div>
         </div>
       </div>
     </div>
